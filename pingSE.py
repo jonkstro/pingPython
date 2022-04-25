fa1609ab9ddd9cdaefe8494a2ec285621c21a3a3
Jonas Castro <103316482+jonkstro@users.noreply.github.com>
2022-04-25 13:26:38 -0300

    Update pingSE.py
    Retirado alguns conteúdos

--- pingSE.py
+++ pingSE.py
@@ -1,25 +1,14 @@
 import os
 import requests
 
-token = "5193651735:AAFPChZ2ZqNm4tUzvF81ZKhQoFbcqSnfCQw"
-chatId = "-1001793924653"
+token = "qNm4tUzvF81ZKhQoFbcqSnfCQw"
+chatId = "-10653"
 
-#URL = "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chatId+"&text=<MENSAGEM>"
 URL = "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chatId+"&text="
 
-ipList = [['10.254.0.1', 'Serv. THE'],
-          ['10.75.17.100','SE TBD'],
-          ['10.75.17.101', 'DJ-13C1'],
-          ['10.75.17.102', 'DJ-13C2'],
-          ['10.75.17.103', 'DJ-12S1'],
-          ['10.75.17.104', 'DJ-12S2'],
-          ['10.75.17.105', 'DJ-12S3'],
-          ['10.75.17.106', 'DJ-12S4'],
-          ['10.75.17.107', 'DJ-12S8'],
-          ['10.75.17.108', 'TF-03T1'],
-          ['10.75.17.109', 'DJ-12T1'],
-          ['10.75.17.110', 'TF-03T2'],
-          ['10.75.17.111', 'DJ-12T2']            
+ipList = [['10.0.0.1', 'SE'],
+          ['10.0.0.2', 'TF-02T1'],
+          ['10.0.0.3', 'TF-02T2']            
         ]
 for ip in ipList:
     response = os.popen("ping "+ ip[0] +" -n 4").read()
